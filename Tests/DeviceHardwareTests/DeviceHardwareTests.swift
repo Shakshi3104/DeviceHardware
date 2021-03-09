d633dbeb66c32b148136f1c77c8acb135ecfa386
Shakshi3104 <watchosastrom3104@gmail.com>
2021-03-09 13:24:27 +0900

    Tested on M1 Mac

--- Tests/DeviceHardwareTests/DeviceHardwareTests.swift
+++ Tests/DeviceHardwareTests/DeviceHardwareTests.swift
@@ -4,45 +4,40 @@ import XCTest
 final class DeviceHardwareTests: XCTestCase {
     
     func testModelIdentifier() {
-//        XCTAssertEqual(MacDeviceHardware.deviceHardware.modelIdentifier, "MacBookAir7,2")
-        XCTAssertEqual(MacDeviceHardware.deviceHardware.modelIdentifier, "MacBookPro15,2")
-        XCTAssertEqual(UIDeviceHardware.deviceHardware.modelIdentifier, "x86_64")
+        XCTAssertEqual(MacDeviceHardware.deviceHardware.modelIdentifier, "MacBookPro17,1")
+        XCTAssertEqual(UIDeviceHardware.deviceHardware.modelIdentifier, "arm64")
     }
     
     func testModelName() {
-//        XCTAssertEqual(MacDeviceHardware.deviceHardware.modelName, "MacBook Air (13-inch, Early 2015)")
-//        XCTAssertNotEqual(MacDeviceHardware.deviceHardware.modelName, "MacBook Air (13-inch, 2017)")
-        XCTAssertEqual(MacDeviceHardware.deviceHardware.modelName, "MacBook Pro (13-inch, 2018, Four Thunderbolt 3 ports)")
-        XCTAssertNotEqual(MacDeviceHardware.deviceHardware.modelName, "MacBook Pro (13-inch, 2019, Four Thunderbolt 3 ports)")
+        XCTAssertEqual(MacDeviceHardware.deviceHardware.modelName, "MacBook Pro (13-inch, M1, 2020)")
     }
     
     func testProcessorName() {
-//        XCTAssertEqual(MacDeviceHardware.deviceHardware.processorName, "Intel Core i5-5250U")
-        XCTAssertEqual(MacDeviceHardware.deviceHardware.processorName, "Intel Core i5-8259U")
+        XCTAssertEqual(MacDeviceHardware.deviceHardware.processorName, "Apple M1")
     }
     
     func testCpu() {
-//        XCTAssertEqual(MacDeviceHardware.deviceHardware.cpu, "1.60GHz 2-core")
-        XCTAssertEqual(MacDeviceHardware.deviceHardware.cpu, "2.30GHz 4-core")
+        XCTAssertEqual(MacDeviceHardware.deviceHardware.cpu, "3.2GHz 8-core")
     }
     
     func testGpu() {
-//        XCTAssertEqual(MacDeviceHardware.deviceHardware.gpu, "Intel Iris Graphics 6000")
-        XCTAssertEqual(MacDeviceHardware.deviceHardware.gpu, "Intel Iris Plus Graphics 655")
+        XCTAssertEqual(MacDeviceHardware.deviceHardware.gpu, "Apple M1")
     }
     
     func testNeuralEngine() {
-        XCTAssertEqual(MacDeviceHardware.deviceHardware.neuralEngine, "None")
+        XCTAssertEqual(MacDeviceHardware.deviceHardware.neuralEngine, "16-core")
     }
     
     func testProcessorCount() {
-//        XCTAssertEqual(MacDeviceHardware.deviceHardware.processorCount, 4)
         XCTAssertEqual(MacDeviceHardware.deviceHardware.processorCount, 8)
     }
     
     func testRamString() {
-//        XCTAssertEqual(MacDeviceHardware.deviceHardware.ramString, "8GB")
         XCTAssertEqual(MacDeviceHardware.deviceHardware.ramString, "16GB")
+    }
+    
+    func testUINeuralEngine() {
+        XCTAssertEqual(UIDeviceHardware.deviceHardware.neuralEngine, "N/A")
     }
     
     func testCallPerformance() {
